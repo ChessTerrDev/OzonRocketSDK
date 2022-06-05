0e62427f2542c4f7d521d936252c76829a5b9b60
Roman <chess.terr.dev@yandex.ru>
2022-06-05 12:07:57 +0300

    first commit, stucture project

---
+++ config/GetConfig.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'path_api' => 'https://api-stg.ozonru.me/principal-auth-api/connect/token',
+    'client_id' => 'ApiTest_11111111-1111-1111-1111-111111111111',
+    'grant_type' => 'client_credentials'
+];
